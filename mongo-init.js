9a17d17e80fa3175fdd195259f8d09a6c8f72e65
Zyklon B <ahrimantaroth@gmail.com>
2022-12-11 21:21:28 +0400

    Prepared to release

--- mongo-init.js
+++
@@ -1,7 +0,0 @@
-print('Started Adding the Car_Main');
-db.createUser({
-  user: 'carAdmin',
-  pwd: 'Kin11001Mk?',
-  roles: [{ role: 'readWrite', db: 'car_main' }],
-});
-print('End Adding the User Roles.');
